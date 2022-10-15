2eb0de0116c1287cff124d108d40a10af58ab468
pawelna23 <pawelunawrocki@protonmail.com>
2022-10-14 20:02:18 -0700

    added validate request and signup tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,12 +1,66 @@
 import request from "supertest";
 import { app } from "../../app";
 
-it("returns 201 after successful signup", async () => {});
+it("returns 201 after successful signup", async () => {
+  return await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "testpassword",
+    })
+    .expect(201);
+});
 
-it("returns 401 with an invalid email", async () => {});
+it("returns 401 with an invalid email", async () => {
+  return await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "testtest.com",
+      password: "testpassword",
+    })
+    .expect(401);
+});
 
-it("returns a 401 with an invalid password", async () => {});
+it("returns a 401 with an invalid password", async () => {
+  return await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "word",
+    })
+    .expect(401);
+});
 
-it("returns a 401 with an empty body", async () => {});
+it("returns a 401 with an empty body", async () => {
+  return await request(app).post("/api/users/signup").send({}).expect(401);
+});
 
-it("sets a cookie with valid signup", async () => {});
+it("duplicate emails are rejected", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "testpassword",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "testpassword",
+    })
+    .expect(401);
+});
+
+it("sets a cookie with valid signup", async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "testpassword",
+    })
+    .expect(201);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,22 @@
 import express from "express";
+import { User } from "../models/user";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+import { validateRequest } from "../middleware/validate-request";
 
 const router = express.Router();
 
-router.get("/api/auth/signup", (req, res) => {});
+router.post(
+  "/api/auth/signup",
+  [
+    body("email").isEmail().withMessage("email must be a valid email"),
+    body("password")
+      .trim()
+      .isLength({ min: 6, max: 20 })
+      .withMessage("Password must be at least 6 characters"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {}
+);
 
 export { router as signupRouter };
