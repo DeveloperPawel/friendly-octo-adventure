cc689bbd542b62ff74a91ba8013cd711b9d13378
pawelna23 <pawelunawrocki@protonmail.com>
2022-10-16 20:47:43 -0700

    reupdated new patient

--- patient/src/routes/__test__/new-patient.test.ts
+++ patient/src/routes/__test__/new-patient.test.ts
@@ -3,14 +3,10 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("creates new patient", async () => {
-  const patientId = new mongoose.Types.ObjectId().toHexString();
-  const response = await request(app)
+  await request(app)
     .post(`/api/patient/new`)
     .send({
-      patientId,
       discharge: new Date(),
     })
     .expect(201);
-
-  expect(response.body).toEqual(patientId);
 });
