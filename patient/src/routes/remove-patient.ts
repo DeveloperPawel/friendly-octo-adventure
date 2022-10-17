53c1e8a27d9712ae6c935a12c0db098139f55d01
pawelna23 <pawelunawrocki@protonmail.com>
2022-10-17 01:16:10 -0700

    updated remove patient

--- patient/src/routes/remove-patient.ts
+++ patient/src/routes/remove-patient.ts
@@ -1,16 +1,35 @@
 import { providerAuth } from "./../../../common/src/middleware/provider-auth";
 import express, { Request, Response } from "express";
-import { activeUser } from "../../../common/src/middleware/active-user";
+import { Patient } from "../model/patient";
+import { Provider } from "../model/provider";
+import { NotFoundError } from "../../../common/src/errors/not-found-error";
 
 const router = express.Router();
 
 router.post(
   "/api/patient/remove",
   providerAuth,
-  (req: Request, res: Response) => {
-    // req.session = null;
+  async (req: Request, res: Response) => {
+    const { providerId, patientId } = req.body;
 
-    res.status(200).send({});
+    const foundPatient = await Patient.findOne({ id: patientId });
+
+    if (!foundPatient) {
+      throw new NotFoundError();
+    }
+
+    const foundProvider = await Provider.findOne({ id: providerId });
+
+    if (!foundProvider) {
+      throw new NotFoundError();
+    }
+
+    foundProvider.patients = foundProvider.patients!.filter(function (patient) {
+      return patient === patientId;
+    });
+    await foundProvider.save();
+
+    res.status(202).send(foundProvider);
   }
 );
 
