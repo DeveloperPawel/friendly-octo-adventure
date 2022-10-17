1a408fab1a6ede818109bb8639a568e563f16d7d
pawelna23 <pawelunawrocki@protonmail.com>
2022-10-16 21:54:04 -0700

    completed one-patient

--- patient/src/routes/one-patient.ts
+++ patient/src/routes/one-patient.ts
@@ -1,15 +1,21 @@
 import { providerAuth } from "./../../../common/src/middleware/provider-auth";
 import express, { Request, Response } from "express";
+import { Patient } from "../model/patient";
+import { NotFoundError } from "../../../common/src/errors/not-found-error";
 
 const router = express.Router();
 
 router.get(
   "/api/patient/patient/:patientId",
   providerAuth,
-  (req: Request, res: Response) => {
-    req.session = null;
+  async (req: Request, res: Response) => {
+    const foundPatient = await Patient.findById(req.params.patientId);
 
-    res.status(200).send({});
+    if (!foundPatient) {
+      throw new NotFoundError();
+    }
+
+    res.status(200).send(foundPatient);
   }
 );
 
