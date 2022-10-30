b1073893595e11e224a3a8cf2807a514c1bf43c4
pawelna23 <pawelunawrocki@protonmail.com>
2022-10-29 23:56:09 -0700

    updated patient-orders route

--- orders/src/routes/provider-patient-orders.ts
+++ orders/src/routes/provider-patient-orders.ts
@@ -1,16 +1,20 @@
 import express, { Request, Response } from "express";
 import { providerAuth } from "@mimenu/common";
-import { Order } from "../models/order";
+import { Patient } from "../models/patient";
 
 const router = express.Router();
 
-router.get(
+router.post(
   "/api/order/patient-orders",
   providerAuth,
   async (req: Request, res: Response) => {
     const { patientList } = req.body;
 
-    res.status(200).send({});
+    const foundPatients = await Patient.find({
+      patientId: { $in: patientList },
+    }).populate({ path: "orders", model: "Order" });
+
+    res.status(200).send(foundPatients);
   }
 );
 
