01fb2c2ad7640ddc10e243e0a8d7c574aeecada6
pawelna23 <pawelunawrocki@protonmail.com>
2022-10-30 00:28:14 -0700

    updated patient accordian test to reflect actual data it will receive

--- client/src/__test__/patient-accordian.test.jsx
+++ client/src/__test__/patient-accordian.test.jsx
@@ -1,13 +1,200 @@
 import { render, screen } from "@testing-library/react";
 import { PatientAccordian } from "../../components/provider-patient-accordian";
+import { formatDateAlpha } from "../../functions/date";
 import "@testing-library/jest-dom";
 
-test('renders all of the patients', () => {
-    render(<PatientAccordian patients={} />);
+const patientData = [
+  {
+    patientId: "635e207d10c248095e78ce51",
+    orders: [
+      {
+        id: "635e207d10c248095e78ce52",
+        breakfast: "635e207d10c248095e78ce4e",
+        lunch: "635e207d10c248095e7sd8ce4e",
+        dinner: "635e2ssd07d10c248095e7sd8ce4e",
+        date: new Date(formatDateAlpha(new Date())),
+        patientId: "635e207d10c248095e78ce51",
+        orderId: "635e207d10c248095e78ce50",
+      },
+    ],
+    preferences: [],
+    restrictions: [],
+    id: "635e207d10c248095e78ce54",
+  },
+  {
+    patientId: "635e207d10c248095e78ce57",
+    orders: [
+      {
+        id: "635e207d10c248095e78ce58",
+        breakfast: "s635e207d10c248095e78ce4e",
+        lunch: "635e2s07d10c248095e7sd8ce4e",
+        dinner: "635e2ssd0s7d10c248095e7sd8ce4e",
+        date: new Date(formatDateAlpha(new Date())),
+        patientId: "635e207d10c248095e78ce57",
+        orderId: "635e207d10c248095e78ce56",
+      },
+    ],
+    preferences: [],
+    restrictions: [],
+    id: "635e207d10c248095e78ce5a",
+  },
+  {
+    patientId: "635e207d10c248095e78ce5d",
+    orders: [
+      {
+        id: "635e207d10c248095e78ce5e",
+        breakfast: "s635e207d10c248095e78ce4e",
+        lunch: "635e2s07d10c248095e7sd8ce4e",
+        dinner: "635e2ssd0s7d10c248095e7sd8ce4e",
+        date: new Date(formatDateAlpha(new Date())),
+        patientId: "635e207d10c248095e78ce5d",
+        orderId: "635e207d10c248095e78ce5c",
+      },
+    ],
+    preferences: [],
+    restrictions: [],
+    id: "635e207d10c248095e78ce60",
+  },
+  {
+    patientId: "635e207d10c248095e78ce63",
+    orders: [
+      {
+        id: "635e207d10c248095e78ce64",
+        breakfast: "s635e207d10c248095e78ce4e",
+        lunch: "635e2s07d10c248095e7sd8ce4e",
+        dinner: "635e2ssd0s7d10c248095e7sd8ce4e",
+        date: new Date(formatDateAlpha(new Date())),
+        patientId: "635e207d10c248095e78ce63",
+        orderId: "635e207d10c248095e78ce62",
+      },
+    ],
+    preferences: [],
+    restrictions: [],
+    id: "635e207d10c248095e78ce66",
+  },
+];
 
+const dayData = {
+  date: new Date(formatDateAlpha(new Date())),
+  dayId: "q24j35oi356ouh3o4ih534ll",
+  breakfast: [
+    {
+      entreeId: "s635e207d10c248095e78ce4e",
+      name: "eggs",
+      foodItems: [
+        {
+          foodItemId: "asdfasdfasdf",
+          name: "egg",
+          ingredients: [
+            {
+              ingredientId: "asasdflientient",
+              name: "egg",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      entreeId: "234n4l345n8888324ln54",
+      name: "bread",
+      foodItems: [
+        {
+          foodItemId: "asdfasdf9asdf",
+          name: "bread",
+          ingredients: [
+            {
+              ingredientId: "asasdfli9999entient",
+              name: "bread",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  lunch: [
+    {
+      entreeId: "635e2s07d10c248095e7sd8ce4e",
+      name: "sandwich",
+      foodItems: [
+        {
+          foodItemId: "asdfasdfasdf",
+          name: "egg",
+          ingredients: [
+            {
+              ingredientId: "asasdflientient",
+              name: "egg",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      entreeId: "234n4l345n8888324ln54",
+      name: "bread",
+      foodItems: [
+        {
+          foodItemId: "asdfasdf9asdf",
+          name: "bread",
+          ingredients: [
+            {
+              ingredientId: "asasdfli9999entient",
+              name: "bread",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  dinner: [
+    {
+      entreeId: "234n4l345n324lng54",
+      name: "steak and eggs",
+      foodItems: [
+        {
+          foodItemId: "asdfasdfasdf",
+          name: "egg",
+          ingredients: [
+            {
+              ingredientId: "asasdflientient",
+              name: "egg",
+            },
+          ],
+        },
+        {
+          foodItemId: "aggsdfasdfasdf",
+          name: "steak",
+          ingredients: [
+            {
+              ingredientId: "asasdflggientient",
+              name: "beef",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      entreeId: "635e2ssd0s7d10c248095e7sd8ce4e",
+      name: "bread",
+      foodItems: [
+        {
+          foodItemId: "asdfasdf9asdf",
+          name: "bread",
+          ingredients: [
+            {
+              ingredientId: "asasdfli9999entient",
+              name: "bread",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+test("renders all of the patients", () => {
+  render(<PatientAccordian patients={patientData} day={dayData} />);
 });
 
-test('title indicates the status of the patients orders', () => {
-    render(<PatientAccordian />);
-
+test("title indicates the status of the patients orders", () => {
+  render(<PatientAccordian patients={patientData} day={dayData} />);
 });
--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -43,7 +43,7 @@ const orderSchema = new mongoose.Schema(
     toJSON: {
       transform(doc, ret) {
         ret.id = ret._id;
-        delete ret.id;
+        delete ret._id;
       },
     },
   }
