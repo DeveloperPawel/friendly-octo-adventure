d2b6f6ff9ba370d012e44b6f7a2f545a8192a861
pawelna23 <pawelunawrocki@protonmail.com>
2022-11-08 00:12:18 -0700

    created test for auth form

--- client/auth/src/__test__/general.test.js
+++ client/auth/src/__test__/general.test.js
@@ -1,1 +1,78 @@
-test("should first", () => {});
+import { render, screen } from "@testing-library/react";
+import { Form } from "../components/Form";
+import UserEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+
+test("renders login form", async () => {
+  render(<Form type="login" />);
+
+  const userNameTextINput = await screen.findAllByRole();
+  const passwordTextInput = await screen.findAllByRole();
+
+  const signupButton = await screen.findAllByRole();
+  const loginButton = await screen.findAllByRole();
+
+  expect(userNameTextINput).toBeDefined();
+  expect(passwordTextInput).toBeDefined();
+  expect(signupButton).toBeDefined();
+  expect(loginButton).toBeDefined();
+});
+
+test("renders signup form", async () => {
+  render(<Form type="signup" />);
+
+  const userNameTextINput = await screen.findAllByRole();
+  const passwordTextInput = await screen.findAllByRole();
+
+  const signupButton = await screen.findAllByRole();
+  const loginButton = await screen.findAllByRole();
+
+  expect(userNameTextINput).toBeDefined();
+  expect(passwordTextInput).toBeDefined();
+  expect(signupButton).toBeDefined();
+  expect(loginButton).toBeDefined();
+});
+
+test("login button disabled when username and pass empty", async () => {
+  render(<Form type="login" />);
+
+  const userNameTextINput = await screen.findAllByRole();
+  const passwordTextInput = await screen.findAllByRole();
+
+  const signupButton = await screen.findAllByRole();
+  const loginButton = await screen.findAllByRole();
+
+  expect(userNameTextINput).toBeDefined();
+  expect(passwordTextInput).toBeDefined();
+  expect(signupButton).toBeDefined();
+  expect(loginButton).toBeDefined();
+
+  expect(loginButton).toBeDisabled();
+
+  UserEvent.type(userNameTextInput, "username");
+  UserEvent.type(passwordTextInput, "password");
+
+  expect(loginButton).toBeEnabled();
+});
+
+test("signup button disabled when the username and password are empty", async () => {
+  render(<Form type="signup" />);
+
+  const userNameTextInput = await screen.findAllByRole();
+  const passwordTextInput = await screen.findAllByRole();
+
+  const signupButton = await screen.findAllByRole();
+  const loginButton = await screen.findAllByRole();
+
+  expect(userNameTextInput).toBeDefined();
+  expect(passwordTextInput).toBeDefined();
+  expect(signupButton).toBeDefined();
+  expect(loginButton).toBeDefined();
+
+  expect(signupButton).toBeDisabled();
+
+  UserEvent.type(userNameTextInput, "username");
+  UserEvent.type(passwordTextInput, "password");
+
+  expect(signupButton).toBeEnabled();
+});
