8ce063464fbc541a62003c3d61fa7136907cea01
pawelna23 <pawelunawrocki@protonmail.com>
2022-11-08 20:28:25 -0700

    updated exports

--- client/common/src/index.js
+++ client/common/src/index.js
@@ -1,4 +1,4 @@
-export * from "./components/chip-card";
-export * from "./components/entree-card";
-export * from "./components/order-card";
-export * from "./components/provider-patient-accordian";
+export { ChipCard } from "./components/chip-card";
+export { EntreeCard } from "./components/entree-card";
+export { OrderCard } from "./components/order-card";
+export { PatientAccordian } from "./components/provider-patient-accordian";
